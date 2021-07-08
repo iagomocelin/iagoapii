91580769de53a104714f6f3927982cb9b1e08661
Iago Mocelin <48732574+iagomocelin@users.noreply.github.com>
2021-07-07 21:57:10 -0300

    sorteio de sugestão com base em user choices pronto
    falta só deixar bonito

--- curso algoritmos/projetofinal/brain.php
+++ curso algoritmos/projetofinal/brain.php
@@ -1,24 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
 <?php
-$csvFile = file('netflix_titles.csv');
-foreach ($csvFile as $line) {
-    $umRegistro = str_getcsv($line);
+    $possibilidades = [];
+    $csvFile = file('netflix_titles.csv');
     
-    }
 
     if(isset($_POST["tipo"]) == true){
         $tipo = $_POST["tipo"];
         echo "TIPO: " . $tipo . "<br>";
     }
-    if(isset($_POST["pais"]) == true && $_POST["pais"] != ""){
-			$pais = $_POST["pais"]; //transformo a seleção em uma variável
-			echo "o pais selecionado foi: " . $pais . "<br>"; //imprimo uma mensagem para o usuário
-		}
-        if(isset($_POST["categoria"]) == true){
+
+    if(isset($_POST["pais"]) == true){
+		$pais = $_POST["pais"]; //transformo a seleção em uma variável
+		echo "o pais selecionado foi: " . $pais . "<br>"; //imprimo uma mensagem para o usuário
+	}
+
+    if(isset($_POST["categoria"]) == true){
         $vetorCategoria = $_POST["categoria"];
         foreach($vetorCategoria as $categoria){
             echo " - " . $categoria . "<br>";
         }
     }
+
     if(isset($_POST["min"]) == true){
         $min = $_POST["min"];
         echo "início do período: " . $min . "<br>";
@@ -28,4 +40,30 @@ foreach ($csvFile as $line) {
         $max = (int)$_POST["max"];
         echo "fim do período: " . $max . "<br>";
     }
+    
+    
+
+    foreach ($csvFile as $line) {
+        $umRegistro = str_getcsv($line);
+        if (!empty($umRegistro[5]) && !empty($umRegistro[10])){
+
+        $categorias = explode(",", $umRegistro[10]);
+        $paiss = ltrim($pais); // precisa tirar o espaço da frente para ele reconhecer o paíde corretamente
+            $tempais = strpos($umRegistro[5], $paiss);
+            $temcategoria = strpos($umRegistro[10], $categoria);
+
+            if ($umRegistro[1] == $tipo && $tempais !== false && $temcategoria!== false && $umRegistro[7] >= $min && $umRegistro[7] <= $max){
+                    $possibilidades[] = $umRegistro;
+
+                }
+
+            }
+        }
+    
+    $sugestao = $possibilidades[rand(0, count($possibilidades))];
+    echo($sugestao[2] . "<br>");
+    echo($sugestao[10] . "<br>");
+    echo($sugestao[7] . "<br>");
 ?>
+</body>
+</html>
--- curso algoritmos/projetofinal/index.php
+++ curso algoritmos/projetofinal/index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-
-    <meta charset="utf-8">
+    <head>
+         <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
 
-</head>
-<body>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+
 
     <?php
     $csvFile = file('netflix_titles.csv');
@@ -26,46 +29,36 @@
                 }    
             }
         }
+    }
 
-        if (!empty($umRegistro[10])){
-        $arrayClassificacoes = str_getcsv($umRegistro[10]); //todas as classificações da linha
-            foreach($arrayClassificacoes as $umaClassificacao){
-                if(in_array($umaClassificacao, $classificacoes) == false && $umaClassificacao != ""  ) {
-
-                    $classificacoes[] = $umaClassificacao;
-                }    
-            }        
-            
-        }   
-        }
+        
 
         sort($paises);
-        sort($classificacoes);
+        $classificacoes = ["Anime Features", "Action & Adventure", "Anime Series", "British TV Shows", "Children & Family Movies", "Classic & Cult TV", "Classic Movies", "Comedies", "Crime TV Shows", "Cult Movies", "Documentaries", "Docuseries", "Dramas", "Faith & Spirituality", "Horror Movies", "Independent Movies", "International Movies", "International TV Shows", "Kids' TV", "Korean TV Shows", "LGBTQ Movies", "Music & Musicals", "Reality TV", "Romantic Movies", "Romantic TV Shows", "Sci-Fi & Fantasy", "Science & Nature TV", "Spanish-Language TV Shows", "Sports", "Movies", "Stand-Up Comedy", "Stand-Up Comedy & Talk Shows", "Teen TV Shows", "Thrillers", "TV Action & Adventure", "TV Comedies", "TV Dramas", "TV Horror", "TV Mysteries", "TV Sci-Fi & Fantasy", "TV Thrillers", "TV Shows"];
     ?>
     
     <div class="container-md">
+
     <form method='post' action='brain.php'>
 
-        <!-- tipo -->
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="tipo" value="Movie" id="flexRadioDefault1">
-            <label class="form-check-label" for="flexRadioDefault1">
-                Movie
-            </label>
-        </div>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input" type="radio" name="tipo" value="TV Show" id="flexRadioDefault2">
-            <label class="form-check-label" for="flexRadioDefault2">
-                TV Show
-            </label>
-        </div>
+    <!-- tipo -->
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="tipo" value="Movie" id="flexRadioDefault1">
+        <label class="form-check-label" for="flexRadioDefault1">
+            Movie
+        </label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="tipo" value="TV Show" id="flexRadioDefault2">
+        <label class="form-check-label" for="flexRadioDefault2">
+            TV Show
+        </label>
+    </div>
 
-        <br>
-
-        <!-- país -->
-        Countries:
+    <!-- país -->
+        <br>Countries:
         <select name='pais'>
-            <option value="">-- any country --</option>"
+        <option value="">-- any country --</option>"
             <?php
                 foreach($paises as $umPais){
                     echo("<option value='" . $umPais . "'>" . $umPais . "</option>");
@@ -73,31 +66,28 @@
             ?>
         </select>
 
-        <br>
+    <br>
+    <!-- categorias -->
+    <label class="col-sm-2 col-form-label">Categories:</label>
+    <div class="col-sm-10">
+    <?php
+        foreach($classificacoes as $umaClassificacao){
+            echo("<div class='form-check form-check-inline'><input name='categoria[]' class='form-check-input' type='checkbox' value= '" . $umaClassificacao . "'><label class='form-check-label' for='flexCheckChecked'>" . $umaClassificacao . "</label></div>");
+        }
+        ?>
+    </div>
+        
 
-        <!-- categorias -->
-        <label class="col-sm-2 col-form-label">Categories:</label>
-        <div class="col-sm-10">
-        <?php
-            foreach($classificacoes as $umaClassificacao){
-                echo("<div class='form-check form-check-inline'><input name='categoria[]' class='form-check-input' type='checkbox' value= '" . $umaClassificacao . "'><label class='form-check-label' for='flexCheckChecked'>" . $umaClassificacao . "</label></div>");
-            }
-            ?>
-        </div>
-            
-        <!-- período -->
-        <div class="min-max-slider" data-legendnum="2">
-            <label>Beginning Year</label>
-            <input id="min" class="min" name="min" type="range" step="1" min="1950" max="2021.5" />
-            <label>Last Year</label>
-            <input id="max" class="max" name="max" type="range" step="1" min="1950" max="2021.5" />
-        </div>
+    <div class="min-max-slider" data-legendnum="2">
+    <label>Beginning Year</label>
+    <input id="min" class="min" name="min" type="range" step="1" min="1950" max="2021.5" />
+    <label>Last Year</label>
+    <input id="max" class="max" name="max" type="range" step="1" min="1950" max="2021.5" />
+</div>
 
-        <!-- submit -->
-        <input name="btnEnviar" type="submit" value="Let's go!">
+    <input name="btnEnviar" type="submit" value="Let's go!">
     </form>
     </div>
-    
     <script src="script.js"></script>
 
 </body>
